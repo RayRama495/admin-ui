f9cabe12a5fea906f4f4d958ac94c6c22357e3ef
RayRama <121413095+ReturnerWolf@users.noreply.github.com>
2024-05-02 11:41:10 +0700

    Uts

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
           </div>
           <div className="item">
             <img
-              src="https://images.unsplash.com/photo-1609505848912-b7c3b8b4beda?q=80&w=1365&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              src="https://www.capradio.org/media/1514627/0417-gray-wolf-P.jpg"
               alt=""
               className="avatar"
             />
